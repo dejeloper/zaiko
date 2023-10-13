68f3c91a47e3ccfbbacb07da0a174afd71218f74
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-10-13 13:08:46 -0500

    Implement TaskCard in Home

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,3 +1,4 @@
+import TaskCard from "@/components/Tasks/TaskCard";
 import { prisma } from "../libs/prisma";
 
 async function loadTasks() {
@@ -9,22 +10,14 @@ async function TasksPage() {
   const tasks = await loadTasks();
 
   return (
-    <main className="min-h-screen w-full bg-gray-900">
+    <main className="h-[calc(100vh-7rem)] w-full bg-gray-900">
       <h1 className="text-center m-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl text-white">
-        Tareas Nuevas
+        Listado de Tareas
       </h1>
       <section className="flex flex-row justify-center text-center m-2 gap-2 py-8 px-4   max-w-screen-xl  lg:py-16">
-        <section className="w-full grid grid-cols-3 gap-3">
+        <section className="w-full grid md:grid-cols-2 lg:grid-cols-3 gap-3">
           {tasks.map((task) => (
-            <div
-              key={task.id}
-              className="block p-6 border rounded-lg shadow bg-gray-800 border-gray-700 hover:bg-gray-700"
-            >
-              <h5 className="mb-2 text-2xl font-bold tracking-tighttext-white">
-                {task.title}
-              </h5>
-              <p className="font-normal text-gray-400">{task.description}</p>
-            </div>
+            <TaskCard key={task.id} task={task} />
           ))}
         </section>
       </section>
